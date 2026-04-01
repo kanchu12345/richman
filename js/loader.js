63103113bece01017d8e0b18383c9734261ad41d
kanchu12345 <44029419+kanchu12345@users.noreply.github.com>
2026-04-01 21:15:37 +0530

    add

--- js/loader.js
+++ js/loader.js
@@ -105,9 +105,10 @@
     const nums = document.querySelectorAll('.stat-item .number');
     if (nums[0]) nums[0].dataset.target = c.founded;
     if (nums[1]) { nums[1].dataset.target = c.members || 400; nums[1].dataset.suffix = '+'; }
-    if (nums[2]) { nums[2].dataset.target = c.districtAwards; nums[2].dataset.suffix = '+'; }
-    if (nums[3]) nums[3].dataset.target = (new Date().getFullYear() - parseInt(c.founded));
-    if (nums[4]) { nums[4].dataset.target = c.alumni || 300; nums[4].dataset.suffix = '+'; }
+    if (nums[2]) { nums[2].dataset.target = c.districtAwards || 20; nums[2].dataset.suffix = '+'; }
+    if (nums[3]) { nums[3].dataset.target = c.multipleDistrictAwards || 5; nums[3].dataset.suffix = '+'; }
+    if (nums[4]) nums[4].dataset.target = (new Date().getFullYear() - parseInt(c.founded));
+    if (nums[5]) { nums[5].dataset.target = c.alumni || 300; nums[5].dataset.suffix = '+'; }
     
     // Clear initial text for animation
     nums.forEach(n => n.textContent = '0');
